1aaa7d0f13cdeecc294a1bdd78411780a7491399
annoying-rambler <annoying-ramber@annoyance.com>
2024-01-30 21:01:23 -0500

    remove refs

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kritchie';
-export const SITE_DESCRIPTION = 'Kritchie\'s Website';
+export const SITE_TITLE = 'Ramblings';
+export const SITE_DESCRIPTION = 'Ramblingse';
